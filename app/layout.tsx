6484896ced04ce92c5e04393f06919390169dedf
Jacob Asper <jacobasper191@gmail.com>
2023-09-08 20:56:30 -0400

    actually fix eslint issue this time

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <script>
-        console.log("hi");
-      </script>
       <body className={inter.className}>{children}</body>
     </html>
   )
