b82c9f21af58880d006c3c405607c06282e04a59
Jacob Asper <jacobasper191@gmail.com>
2023-09-10 04:54:43 -0400

    Add 100Editors heading

--- app/page.tsx
+++ app/page.tsx
@@ -27,6 +27,7 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
+      <h1 className="mb-6 text-3xl">100Editors</h1>
       <TextEditor
         className="w-full"
         modules={{ toolbar: false }}
