2e263dcd012dfbcde2d043060caf06c21554cc8d
Jacob Asper <jacobasper191@gmail.com>
2023-09-15 22:38:43 -0400

    WIP yjs quill setup

--- app/components/TextEditor.tsx
+++ app/components/TextEditor.tsx
@@ -1,58 +1,103 @@
-import dynamic from "next/dynamic";
 import ReactQuill from "react-quill";
+import * as Y from "yjs";
+import { QuillBinding } from "y-quill";
+
 import { useQuery, useMutation } from "convex/react";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { api } from "../../convex/_generated/api";
 
-import type { ReactQuillProps } from "react-quill";
-type OnChange = ReactQuillProps["onChange"];
 
+import type { ReactQuillProps } from "react-quill";
+
+import styles from "./Editor.module.css";
 import "react-quill/dist/quill.snow.css";
 
 const LOCAL_STORAGE_KEY = "local storage key";
 
 export default function TextEditor(props: ReactQuillProps) {
   const inputValue = useQuery(api.input.getInputValue);
-  const mutateValue = useMutation(api.input.updateInputValue);
-  const [text, setText] = useState(
-    localStorage.getItem(LOCAL_STORAGE_KEY) ?? ""
-  );
+  const [text, setText] = useState<Y.Text>();
+  // const [provider, setProvider] = useState<any>();
 
   const isLoading = (query: unknown): query is undefined | null => {
     return query === undefined || query === null;
   };
 
   // update local copy when db updates
-  useEffect(() => {
-    if (!isLoading(inputValue)) {
-      setText(inputValue.text);
-      localStorage.setItem(LOCAL_STORAGE_KEY, inputValue.text);
-    }
-  }, [inputValue]);
+   useEffect(() => {
+    const yDoc = new Y.Doc();
+    const yText = yDoc.getText("quill");
+    // const yProvider = new LiveblocksProvider(room, yDoc);
 
-  const handleOnChange: OnChange = (_value, _delta, _source, editor) => {
-    // update local copy
-    const currentText = editor.getHTML();
-    setText(currentText);
-    localStorage.setItem(LOCAL_STORAGE_KEY, currentText);
+    setText(yText);
+    // setProvider(yProvider);
 
-    // update db when loaded
-    if (!isLoading(inputValue)) {
-      mutateValue({
-        id: inputValue._id,
-        text: currentText,
-      });
-    }
-  };
+    return () => {
+      yDoc?.destroy();
+      // yProvider?.destroy();
+    };
+
+  }, []);
+
+
+  if (!text 
+    // || !provider
+    ) {
+    return null;
+  }
 
   return (
-    <ReactQuill
+    <QuillEditor
       modules={{ toolbar: false }}
       formats={[]}
-      value={text}
+      yText={text}
+      provider={4}
       readOnly={isLoading(inputValue)}
-      onChange={handleOnChange}
       {...props}
     />
+  );
+}
+
+type EditorProps = {
+  yText: Y.Text;
+  provider: any;
+};
+
+function QuillEditor({ yText, provider }: EditorProps) {
+  const reactQuillRef = useRef<ReactQuill>(null);
+  // Set up Yjs and Quill
+  useEffect(() => {
+    let quill: ReturnType<ReactQuill["getEditor"]>;
+    let binding: QuillBinding;
+
+    if (!reactQuillRef.current) {
+      return;
+    }
+
+    quill = reactQuillRef.current.getEditor();
+    binding = new QuillBinding(yText, quill, provider.awareness);
+    return () => {
+      binding?.destroy?.();
+    };
+  }, [yText, provider]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.editorContainer}>
+        <ReactQuill
+          className={styles.editor}
+          placeholder="Start typing here…"
+          ref={reactQuillRef}
+          theme="snow"
+          modules={{
+            toolbar: false,
+            history: {
+              // Local undo shouldn't undo changes from remote users
+              userOnly: true,
+            },
+          }}
+        />
+      </div>
+    </div>
   );
 }
