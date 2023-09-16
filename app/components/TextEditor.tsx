32ceba33e64d9b59a40898389eae67a09f832263
Jacob Asper <jacobasper191@gmail.com>
2023-09-15 23:15:48 -0400

    revert changes that for some reason broke the editor

--- app/components/TextEditor.tsx
+++ app/components/TextEditor.tsx
@@ -1,18 +1,27 @@
 import ReactQuill from "react-quill";
 import * as Y from "yjs";
 import { QuillBinding } from "y-quill";
-// TODO figure out how to make the types work here
+// TODO acutally fix this
 // @ts-ignore because the types don't want to work :(
 import { WebrtcProvider } from "y-webrtc";
+
+import { useQuery } from "convex/react";
 import { useEffect, useRef, useState } from "react";
+import { api } from "../../convex/_generated/api";
 
 import type { ReactQuillProps } from "react-quill";
 
 import styles from "./Editor.module.css";
+import "react-quill/dist/quill.snow.css";
 
 export default function TextEditor(props: ReactQuillProps) {
+  const inputValue = useQuery(api.input.getInputValue);
   const [text, setText] = useState<Y.Text>();
   const [provider, setProvider] = useState<any>();
+
+  const isLoading = (query: unknown): query is undefined | null => {
+    return query === undefined || query === null;
+  };
 
   useEffect(() => {
     const yDoc = new Y.Doc();
@@ -28,7 +37,6 @@ export default function TextEditor(props: ReactQuillProps) {
     };
   }, []);
 
-  // TODO actually error handle?
   if (!text || !provider) {
     return <h1>your code is broken</h1>;
   }
@@ -39,6 +47,7 @@ export default function TextEditor(props: ReactQuillProps) {
       formats={[]}
       yText={text}
       provider={provider}
+      readOnly={isLoading(inputValue)}
       {...props}
     />
   );
