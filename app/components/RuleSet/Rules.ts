099ca6cbf5a3e266d39f98f45a893a6561d379df
nmpereira <nikhilpereira25@gmail.com>
2023-09-18 21:04:00 -0400

    Update rules and improve readability

--- app/components/RuleSet/Rules.ts
+++ app/components/RuleSet/Rules.ts
@@ -1,3 +1,10 @@
+import {
+  animals,
+  colorNames,
+  fairyTaleBeginnings,
+  monthsNames,
+} from "./ValidationLists";
+
 export interface Rule {
   name: string;
   desciption: string;
@@ -14,19 +21,6 @@ export const Rules: RuleStore = [
     desciption:
       'Must start with a fairy tale beginning like "Once upon a time", "In a land far far away" or "Long ago in a kingdom far far away"',
     validation: (text: string) => {
-      const fairyTaleBeginnings = [
-        "once upon a time",
-        "once upon a time in a land far away",
-        "in a land far far away",
-        "in a kingdom far far away",
-        "long ago in a land far far away",
-        "Long ago in a kingdom far far away",
-        "Once upon a time in a land far away",
-        "Once upon a time in a land far far away",
-        "Once upon a time in a kingdom far away",
-        "Once upon a time in a kingdom far far away",
-      ];
-
       const firstSentence = (text: string) =>
         text.toLowerCase().slice(0, 100).replace(/\n$/, "").trim();
 
@@ -74,46 +68,129 @@ export const Rules: RuleStore = [
     name: "Animal count",
     desciption: "Must have at least 3 animals",
     validation: (text: string) => {
-      const animals = [
-        "Dog",
-        "Doggy",
-        "Pup",
-        "Puppy",
-        "Canine",
-        "Cat",
-        "Kitty",
-        "Kitten",
-        "Fish",
-        "Fishy",
-        "Bird",
-        "Birb",
-        "Mouse",
-        "Mice",
-        "Rat",
-        "Ratty",
-        "Rabbit",
-        "Bunny",
-        "Hamster",
-        "Guinea Pig",
-        "Snake",
-        "Serpent",
-        "Turtle",
-        "Tortoise",
-        "Lizard",
-        "Frog",
-        "Amphibian",
-      ];
-
       const cleanedText = text
         .toLowerCase()
         .replace(/[^a-z ]/g, "")
-        .replace(/\s+/g, " ")
-        .trim();
+        .replace(/\s+/g, " ");
 
       return (
-        cleanedText.split(" ").filter((word) => animals.includes(word))
+        animals.filter((animal) => cleanedText.includes(animal.toLowerCase()))
           .length >= 3
       );
+    },
+  },
+  {
+    name: "add your lucky number",
+    desciption: "Must have your lucky number",
+    validation: (text: string) => {
+      return /\d/.test(text);
+    },
+  },
+  {
+    name: "add your favourite colour",
+    desciption: "Must have your favourite colour",
+    validation: (text: string) => {
+      const cleanedText = text
+        .toLowerCase()
+        .replace(/[^a-z ]/g, "")
+        .replace(/\s+/g, " ");
+
+      return (
+        colorNames.filter((color) => cleanedText.includes(color.toLowerCase()))
+          .length >= 1
+      );
+    },
+  },
+  {
+    name: "Capitalize Ts",
+    desciption: "Must capitalize all Ts",
+    validation: (text: string): boolean => {
+      // should not contain capital T but should contain lowercase t for every occurance of t
+
+      const includesCapitalT = text.includes("T");
+      const includesLowercaseTs = !text.includes("t");
+
+      return includesCapitalT && includesLowercaseTs;
+    },
+  },
+  {
+    name: "month names",
+    desciption: "Must have a month name (January, February, etc)",
+    validation: (text: string) => {
+      const cleanedText = text
+        .toLowerCase()
+        .replace(/[^a-z ]/g, "")
+        .replace(/\s+/g, " ");
+
+      return (
+        monthsNames.filter((month) => cleanedText.includes(month.toLowerCase()))
+          .length >= 1
+      );
+    },
+  },
+  {
+    name: "Today's day of the week",
+    desciption: "Must have today's day of the week (Monday, Tuesday, etc)",
+    validation: (text: string) => {
+      // should match monday or mon
+      const today = new Date();
+      const day = today.toLocaleString("default", { weekday: "long" });
+      const dayShort = today.toLocaleString("default", { weekday: "short" });
+
+      return (
+        text.toLowerCase().includes(day.toLowerCase()) ||
+        text.toLowerCase().includes(dayShort.toLowerCase())
+      );
+    },
+  },
+  {
+    name: "Some sentences should start with 'the'",
+    desciption: "Must have atleast 3 sentences that start with 'the'",
+    validation: (text: string) => {
+      // split sentences based on . ! ? \n
+      const theSentences = text
+        .split(/\.|\!|\?|\n/)
+        .map((sentence) => sentence.trim())
+        .filter((sentence) => sentence.toLowerCase().startsWith("the "));
+
+      // only keep sentences that start with the
+
+      return theSentences.length >= 3;
+    },
+  },
+  {
+    name: "Sentences start with a capital letter",
+    desciption: "All sentences must start with a capital letter (A-Z)",
+    validation: (text: string) => {
+      const sentences = text
+        .split(/\.|\!|\?|\n/)
+        .filter((sentence) => sentence.trim() !== "")
+        .map((sentence) => sentence.trim());
+      if (sentences) {
+        return sentences.every((sentence) => {
+          const firstLetter = sentence.trim()[0];
+          return firstLetter === firstLetter.toUpperCase();
+        });
+      }
+      return false;
+    },
+  },
+  {
+    name: "Must have 10 lines",
+    desciption: "Must have 10 lines",
+    validation: (text: string) => {
+      const lines = text.split("\n");
+      return lines.length >= 10;
+    },
+  },
+  {
+    name: "Blank lines every 5",
+    desciption: "Must have blank lines on every 5th line",
+    validation: (text: string) => {
+      // after every 5th line there should be 2 or more line breaks
+      const lines = text.split("\n");
+      const every5thLine = lines.filter((line, i) => (i + 1) % 5 === 0);
+      return every5thLine.every((line) => line === "");
     },
   },
 ];
