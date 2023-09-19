f78fd727ab4b1ae004b2a083d87a31a48a97057d
Mario Nikhil Pereira <45009203+nmpereira@users.noreply.github.com>
2023-09-19 00:27:13 -0400

    Updated instructions on homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,10 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center p-24">
       <h1 className="mb-6 text-3xl">100Editors</h1>
-      <div className="flex justify-center content-center flex-row gap-5">
-        rooms
+      <div className="flex justify-center content-center items-center flex-col gap-5">
+        <p>Go to a room by adding a slug to the URL</p>
+        <p>For Example: /room1 or /room2</p>
+        <p>The editor is now shared per room</p>
       </div>
     </main>
   );
