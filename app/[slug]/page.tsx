855d01fcd81da6c2e106fdee82439c003cbff53b
Jacob Asper <jacobasper191@gmail.com>
2023-09-19 20:23:59 -0400

    add timer component and placeholder users online

--- app/[slug]/page.tsx
+++ app/[slug]/page.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import dynamic from "next/dynamic";
+import Timer from "../components/Timer/Timer";
 
 // we must disable SSR since ReactQuill attempts to access the `document`
 const DocumentWrapper = dynamic(() => import("../components/DocumentWrapper"), {
@@ -18,11 +19,19 @@ export default function Page(props: PageParams) {
 
   return (
     <main className="flex flex-col items-center pt-4">
-      <h1 className="mb-6 text-3xl">100Editors</h1>
-      <h3 className="mb-6 text-2xl">
-        {params.slug ? `Room: ${params.slug}` : ""}
-      </h3>
-      <DocumentWrapper slug={params.slug} />
+      <div>
+        <div className="flex items-end justify-between py-10">
+          <h1 className="text-4xl">100Editors</h1>
+          <div>
+            <h3 className="text-2xl">
+              {params.slug ? `Room: ${params.slug}` : ""}
+            </h3>
+            <h3>Editors Online: 2</h3>
+            <Timer start={Date.now()} />
+          </div>
+        </div>
+        <DocumentWrapper slug={params.slug} />
+      </div>
     </main>
   );
 }
